e362eabe4bd2a878caa970f062fd91d7fec9b9a5
kosukeKK <kosuke.kato@jmty.jp>
2017-11-26 00:01:55 +0900

    data

--- tutorial/app/javascript/packs/sample/actions/index.tsx
+++ tutorial/app/javascript/packs/sample/actions/index.tsx
@@ -1,6 +1,6 @@
-let nextNum :number = 0
-let texts :Array<string> = []
-export const addCr = (text :string) =>  {
-    texts.push(text)
-    return {type: 'ADD_COUNTER', id: nextNum++, texts}
+let nextNum: number = 0;
+let texts: Array<string> = [];
+export const addCounterAction = (text: string) => {
+    texts.push(text);
+    return { type: 'ADD_COUNTER', id: nextNum++, texts };
 };
